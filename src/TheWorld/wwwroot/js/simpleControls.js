a70931a5d23d23280b7d001ddb6260a0e61d0c8f
mitchard@gmail.com <Lomwymad1!>
2016-04-16 11:26:24 -0400

    broken - angular module

---
+++ src/TheWorld/wwwroot/js/simpleControls.js
@@ -0,0 +1,14 @@
+// simpleControls.js
+
+(function () {
+    "use strict";
+
+    angular.module("simpleControls", [])
+    .directive("waitCursor", waitCursor);
+
+    function waitCursor() {
+        return {
+            templateUrl: "/views/waitCursor.html"
+        };
+    }
+})();
